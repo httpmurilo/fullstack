5d709b9d16ecfbdfcf3ab50bcc7fb9b6c3da3b2c
httpmurilo <muriloeduardo1997@outlook.com>
2021-09-07 11:51:05 -0300

    basic static components

---
+++ projeto/frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/httpmurilo" target="_blank" rel="noreferrer">Murilo Eduardo</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/muriloeduardo__" target="_blank" rel="noreferrer">@muriloeduardo__</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
